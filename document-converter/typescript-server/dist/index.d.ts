11b36a894d24d380e7eb699b75417482dcceff34
renjismzy <renjismz@gmail.com>
2025-08-31 16:16:45 +0800

    Delete document-converter directory

--- document-converter/typescript-server/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { DocumentConverterServer } from './document-converter.js';
-declare function main(): Promise<void>;
-export { DocumentConverterServer };
-export default main;
-//# sourceMappingURL=index.d.ts.map
